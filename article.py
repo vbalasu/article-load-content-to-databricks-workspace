352e76999613aa29d05b4ae29fdc581abaf6856c
vbalasu <vbalasu>
2022-08-28 16:35:16 +0000

    Initial commit

---
+++ article.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # How To Load Content Into A Databricks Workspace
+# MAGIC 
+# MAGIC This article describes a few methods you can use to load content into your Databricks workspace.
+# MAGIC 
+# MAGIC The content can consists of several types:
+# MAGIC - Notebooks
+# MAGIC - SQL Dashboards and its related queries
+# MAGIC - Data: tables and views
+# MAGIC - Delta Live Tables (DLT) Pipelines
+# MAGIC 
+# MAGIC The above list is illustrative, but not intended to be comprehensive
+
+# COMMAND ----------
+
+
